32baeeeb835723b55120f8ba70c2138e9ab732e0
umit.tasdemirr48 <umit.tasdemirr48@gmail.com>
2025-12-26 08:51:13 +0000

    Fix: Update theme switch colors to match app style

--- src/presentation/components/profile/SettingsBottomSheet.tsx
+++ src/presentation/components/profile/SettingsBottomSheet.tsx
@@ -62,8 +62,8 @@ export const SettingsBottomSheet = forwardRef<BottomSheet, SettingsBottomSheetPr
               <Switch
                 value={isDark}
                 onValueChange={onThemeToggle}
-                trackColor={{ false: '#767577', true: '#81b0ff' }}
-                thumbColor={isDark ? '#f5dd4b' : '#f4f3f4'}
+                trackColor={{ false: '#767577', true: '#34C759' }}
+                thumbColor={'#f4f3f4'}
                 ios_backgroundColor="#3e3e3e"
               />
             </View>
